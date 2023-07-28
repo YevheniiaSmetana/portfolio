9d54af2a432af43c17c6eade263deecb426ff460
Yevheniia Smetana <zhenjasmetana@gmail.com>
2023-07-28 19:07:56 +0200

    Initial commit

---
+++ src/assets/index.js
@@ -0,0 +1,50 @@
+import design from "./design.png";
+import uxui from "./uxui.png";
+import react from "./react.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import linkedin from "./linkedin.png";
+import behance from "./behance.png";
+import mail from "./mail.png";
+
+import css from "./tech/css.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import githubblack from "./tech/githubblack.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import reactjs from "./tech/reactjs.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+
+import itcoty from "./company/IT-Coty.png";
+import code from "./company/code.png";
+
+import harpfestivalwebsite from "./harpfestivalwebsite.png";
+
+export {
+  design,
+  uxui,
+  react,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  figma,
+  git,
+  githubblack,
+  html,
+  javascript,
+  reactjs,
+  tailwind,
+  typescript,
+  itcoty,
+  code,
+  harpfestivalwebsite,
+  linkedin,
+  behance,
+  mail,
+};
---
+++ src/components/Tech.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import { BallCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { technologies } from "../constants";
+
+const Tech = () => {
+  // Разделяем массив на два
+  const technologiesTop = technologies.slice(0, 5);
+  const technologiesBottom = technologies.slice(5);
+
+  return (
+    <>
+      <div className='flex flex-row flex-wrap justify-center gap-10'>
+        {technologiesTop.map((technology) => (
+          <div className='w-28 h-28 flex flex-col items-center justify-center' key={technology.name}>
+            <BallCanvas icon={technology.icon} />
+            <p className='mt-2'>{technology.name}</p>
+          </div> 
+        ))}
+      </div>
+      <div className='flex flex-row flex-wrap justify-center gap-10 mt-4'>
+        {technologiesBottom.map((technology) => (
+          <div className='w-28 h-28 flex flex-col items-center justify-center' key={technology.name}>
+            <BallCanvas icon={technology.icon} />
+            <p className='mt-2'>{technology.name}</p>
+          </div>        
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(Tech, "");
